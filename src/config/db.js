54640b2b9c3e352f3fc974dec471ebe2ba135184
Master-mohit <mohitmansigupta9098@gmail.com>
2026-09-03 21:28:23 +0530

    connect to DB

---
+++ src/config/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+async function connectDB(){
+  try{
+    await mongoose.connect(process.env.MONGO_URI)
+  }
+  catch(err){
+    console.error(err.message);
+  }
+}
+
+
+module.exports = connectDB;
