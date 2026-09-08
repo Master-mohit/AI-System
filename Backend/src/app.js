85fd873b05de5e4101100ef00b7891e072a6dcb8
Master-mohit <mohitmansigupta9098@gmail.com>
2026-09-08 16:25:31 +0530

    added loaded state using context

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const authrouters = require('./routes/auth.routes');
 const cookie = require('cookie-parser');
+const cors = require('cors');
 
 const app = express();
 app.use(cookie());
-
 app.use(express.json());
+app.use(cors({
+    origin: "http://localhost:5173",
+     Credential: true
+    
+}))
 
 app.use('/api/auth', authrouters);
 
--- Frontend/src/features/auth/pages/Login.jsx
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -1,12 +1,29 @@
 import "../auth.form.scss"
 import { useNavigate, Link } from 'react-router'
+import { useAuth } from '../hooks/useAuth'
+import { useState } from "react"
 
 const Login = () => {
 
     const navigate = useNavigate()
 
+   const { loading, handleLogin } = useAuth()
+
+   const [email, setEmail] = useState("")
+   const [password, setPassword] = useState("")
+
        const handleSubmit = async (e) => {
         e.preventDefault()
+       await handleLogin({email, password})
+       navigate('/')
+    }
+
+    if(loading){
+        return (
+            <main>
+                <h1>Loading..</h1>
+            </main>
+        )
     }
 
     return (
@@ -17,11 +34,13 @@ const Login = () => {
                     <div className="input-group">
                         <label htmlFor="email">Email</label>
                         <input
+                          onChange={(e) => {setEmail(e.target.value)}}
                             type="email" id="email" name='email' placeholder='Enter email address' />
                     </div>
                     <div className="input-group">
                         <label htmlFor="password">Password</label>
                         <input
+                          onChange={(e) => setPassword(e.target.value)}
                             type="password" id="password" name='password' placeholder='Enter password' />
                     </div>
                     <button className='button primary-button' >Login</button>
